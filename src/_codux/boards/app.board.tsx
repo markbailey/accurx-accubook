8c61190a97f34ff6319404e865b3ae84ad704b5e
Mark Bailey <markbailey78@yahoo.co.uk>
2023-04-26 23:30:31 +0100

    Moved app.board

---
+++ src/_codux/boards/app.board.tsx
@@ -0,0 +1,12 @@
+import { createBoard } from '@wixc3/react-board';
+import App from '../../App';
+
+export default createBoard({
+  name: 'App',
+  Board: () => <App />,
+  environmentProps: {
+    canvasWidth: 1024,
+    windowWidth: 1024,
+    windowHeight: 768,
+  },
+});
