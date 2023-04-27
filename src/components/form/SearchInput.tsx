7ecb01324717d623a43b8404b0106b42b4692df4
Mark Bailey <markbailey78@yahoo.co.uk>
2023-04-28 00:23:42 +0100

    Commented out search class for search input field

--- src/components/form/SearchInput.tsx
+++ src/components/form/SearchInput.tsx
@@ -6,7 +6,7 @@ export type SearchInputProps = InputHTMLAttributes<HTMLInputElement>;
 
 const SearchInput = forwardRef((props: SearchInputProps, ref: ForwardedRef<HTMLInputElement>) => {
   const { className: classNameProp, ...otherProps } = props;
-  const className = classNames(styles.formField, styles.search, classNameProp);
+  const className = classNames(styles.formField, classNameProp);
   return (
     <input placeholder="Search" {...otherProps} ref={ref} type="search" className={className} />
   );
