29fcefd5711ba436412128e61cfd144407bf72c3
Mark Bailey <markbailey78@yahoo.co.uk>
2023-04-28 10:40:36 +0100

    Update Loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,15 +1,36 @@
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
+import { mount } from '../utilities/show';
 import styles from '../assets/stylesheets/components/loader.module.scss';
 
-export type LoaderProps = HTMLAttributes<HTMLDivElement>;
+export type LoaderProps = HTMLAttributes<HTMLDivElement> & {
+  show: boolean;
+};
+
+const Spinner = () => <div className={styles.spinner}></div>;
 
 function Loader(props: LoaderProps) {
-  const { className: classNameProp, ...otherProps } = props;
-  const className = classNames(styles.loader, classNameProp);
-  return (
-    <div {...otherProps} className={className}>
-      <span>Loading...</span>
+  const ref = useRef<HTMLDivElement>(null);
+  const [shouldMount, setShouldMount] = useState(false);
+  const [active, setActive] = useState(false);
+  const { className: classNameProp, show, ...otherProps } = props;
+  const className = classNames(styles.loader, active && styles.show, classNameProp);
+
+  useEffect(() => {
+    if (show && !shouldMount) setShouldMount(true);
+    else if (show && !active) setActive(true);
+    else if (!show && active) setActive(false);
+    else if (!show && shouldMount)
+      ref.current?.addEventListener('transitionend', () => setShouldMount(false));
+  }, [show, active, shouldMount]);
+
+  return mount(
+    shouldMount,
+    <div {...otherProps} ref={ref} className={className}>
+      <div className={styles.inner}>
+        <Spinner />
+        <span>Loading...</span>
+      </div>
     </div>
   );
 }
